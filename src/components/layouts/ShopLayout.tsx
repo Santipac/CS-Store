ac12925119937997bac92230d4d97f08b2602715
Santiago <santiagopacini@gmail.com>
2023-05-31 15:18:08 -0300

    footer added

--- src/components/layouts/ShopLayout.tsx
+++ src/components/layouts/ShopLayout.tsx
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import { Spinner } from "../ui";
 import Head from "next/head";
 import dynamic from "next/dynamic";
-
+import NextLink from "next/link";
 const Navbar = dynamic(() => import("@/components/ui/Navbar"), { ssr: false });
 interface Props {
   children: JSX.Element | JSX.Element[];
@@ -39,6 +39,21 @@ export const ShopLayout: React.FC<Props> = ({
           <div className="flex w-full max-w-7xl flex-col px-4">{children}</div>
         </div>
       </main>
+      <footer className="flex w-full flex-col items-center space-y-4 py-8">
+        <h2 className="text-xl font-bold tracking-tighter">
+          This is a side project, NOT a real ecommerce for CSGO Skins.
+        </h2>
+        <span className="text-sm font-medium text-gray-600">
+          This project is developed by{" "}
+          <NextLink
+            href="https://github.com/Santipac"
+            className="text-blue-600 underline"
+          >
+            Santiago
+          </NextLink>
+          .
+        </span>
+      </footer>
     </Suspense>
   );
 };
--- src/components/ui/products/CardProductSkeleton.tsx
+++ src/components/ui/products/CardProductSkeleton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const CardProductSkeleton = () => {
   return (
-    <div className="flex h-[270px] w-full flex-col sm:w-[270px]">
+    <div className="flex h-[270px] w-full flex-col ">
       <div className="h-3/4 w-full animate-pulse rounded-sm bg-gray-300" />
       <div className="mt-4 flex items-center justify-between">
         <div className="h-2 w-10  animate-pulse rounded-sm bg-gray-300" />
