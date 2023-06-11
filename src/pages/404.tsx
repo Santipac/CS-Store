52ed83996360fe161463d5b53b9917d556685b23
Santiago <santiagopacini@gmail.com>
2023-06-11 14:43:52 -0300

    404 page changed

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import NextLink from "next/link";
 const ErrorPage = () => {
   return (
-    <div className="grid h-screen place-content-center bg-white px-4">
+    <div className="grid h-screen place-content-center bg-slate-950 px-4">
       <div className="text-center">
-        <h1 className="text-9xl font-black text-gray-200">404</h1>
+        <h1 className="text-9xl font-black text-gray-300">404</h1>
 
-        <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        <p className="text-2xl font-bold tracking-tight text-gray-500 sm:text-4xl">
           Uh-oh!
         </p>
 
@@ -14,7 +14,7 @@ const ErrorPage = () => {
 
         <NextLink
           href="/"
-          className="mt-6 inline-block rounded bg-arg px-5 py-3 text-sm font-medium text-white hover:bg-sky-300 focus:outline-none focus:ring"
+          className="mt-6 inline-block rounded bg-sky-500 px-5 py-3 text-sm font-medium text-white hover:bg-sky-600 focus:outline-none focus:ring"
         >
           Go Back Home
         </NextLink>
